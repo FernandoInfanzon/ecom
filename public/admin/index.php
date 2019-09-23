828fd628641d487de4863b0b4f304b3233b4c6c8
Luis <web@eze-trip.com>
2019-09-23 16:13:53 -0500

    actualización de carpeta para los archivos el backend.

--- public/admin/index.php
+++ public/admin/index.php
@@ -26,7 +26,20 @@
                     if($_SERVER['REQUEST_URI'] == "/public/admin/" || $_SERVER['REQUEST_URI'] == "/public/admin/index.php" ){
                         include(TEMPLATE_BACK . "/admin_content.php");
                     };
-                
+
+                    if(isset($_GET['orders'])){
+                        include(TEMPLATE_BACK . "/orders.php");
+                    }
+
+                    if(isset($_GET['categories'])){
+                        include(TEMPLATE_BACK . "/categories.php");
+                    }
+                    if(isset($_GET['products'])){
+                        include(TEMPLATE_BACK . "/products.php");
+                    }
+                    if(isset($_GET['add_product'])){
+                        include(TEMPLATE_BACK . "/add_product.php");
+                    }
                 ?>
 
 
